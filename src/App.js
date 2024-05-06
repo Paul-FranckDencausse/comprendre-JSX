6ec0cb888ae0cbf32598276eaf4c3c1cdff028af
Paul-Franck Dencausse <121952248+Paul-FranckDencausse@users.noreply.github.com>
2024-05-06 12:07:59 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+//react c'est du JS , dans ce JS on veut du markup(HTML/CSS) pour le rendu visuel et
+//dans ce markup on veut du JS donc on utilise JSX {}  , logique mais alambiqué je trouve
+const person = {
+  name: "Gregorio Y. Zara",
+  theme: {
+    backgroundColor: "black",
+    color: "pink",
+  },
+};
+
+export default function TodoList() {
+  return (
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src="https://i.imgur.com/7vQD0fPs.jpg"
+        alt="Gregorio Y. Zara"
+      />
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
+    </div>
+  );
+}
